7324896395d0611142e8a02a4b74d753a575c1f6
Priscila Silva <priscilacds_100@yahoo.com.br>
2023-11-21 19:36:42 -0300

    Implementação Deposit

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -84,7 +84,12 @@ public class TrybankLib
     // 5. Construa a funcionalidade de depositar dinheiro
     public void Deposit(int value)
     {
-        throw new NotImplementedException();
+        if (Logged == false) {
+            throw new AccessViolationException("Usuário não está logado");
+        }
+        else {
+            return Bank[loggedUser, 3] += value;
+        }
     }
 
     // 6. Construa a funcionalidade de sacar dinheiro
